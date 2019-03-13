3b818985b6f460a624fb06a9f062d881f2ee56fb
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-03-13 13:41:16 +0530

    Working pass on icon & affix!

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/views/pages/home.vue';
 import Button from '@/views/pages/button.vue';
+import Icon from '@/views/pages/icon.vue';
+import Affix from '@/views/pages/affix.vue';
 
 Vue.use(Router);
 
@@ -18,6 +20,16 @@ export default new Router({
       path: '/button',
       name: 'Button',
       component: Button,
+    },
+    {
+      path: '/icon',
+      name: 'Icon',
+      component: Icon,
+    },
+    {
+      path: '/affix',
+      name: 'Affix',
+      component: Affix,
     },
   ],
 });
---
+++ src/views/pages/icon.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1>Icon</h1>
+    <a-row type="flex">
+      <a-col :span="24">
+        <div class="icon-demos-container">
+          <a-icon type="home" />
+          <a-icon type="setting" theme="filled" />
+          <a-icon type="smile" theme="outlined" />
+          <a-icon type="sync" spin />
+          <a-icon type="loading" />
+        </div>
+        <div class="icon-demos-container">
+          <a-icon type="smile" theme="twoTone" />
+          <a-icon type="heart" theme="twoTone" twoToneColor="#eb2f96" />
+          <a-icon type="check-circle" theme="twoTone" twoToneColor="#52c41a" />
+        </div>
+      </a-col>
+    </a-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Icon',
+  data() {
+    return {
+    };
+  },
+  methods: {
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .icon-demos-container {
+    padding-top: 2rem;
+    padding-bottom: 2rem;
+    border-bottom: 1px dashed #0a0a0a;
+
+    &:first-of-type {
+      padding-top: 0;
+    }
+
+    .anticon {
+      margin-right: 1rem;
+      font-size: 1.5rem;
+    }
+  }
+</style>
